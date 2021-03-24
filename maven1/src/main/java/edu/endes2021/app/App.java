2774e8a5afd12e340bc130e691f1f298c8118881
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-24 17:14:19 +0100

    commit inicial

---
+++ maven1/src/main/java/edu/endes2021/app/App.java
@@ -0,0 +1,13 @@
+package edu.endes2021.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
